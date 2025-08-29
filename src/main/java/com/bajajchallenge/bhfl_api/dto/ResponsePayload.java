f9674d8d3f2e2101ac30149e38703651e2707c9f
theforce1579 <funsuc.wangdu@gmail.com>
2025-08-29 12:06:35 +0530

    Fix: final changes

--- src/main/java/com/bajajchallenge/bhfl_api/dto/ResponsePayload.java
+++ src/main/java/com/bajajchallenge/bhfl_api/dto/ResponsePayload.java
@@ -1,13 +1,14 @@
 package com.bajajchallenge.bhfl_api.dto;
 
 import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor // Lombok: Creates a no-argument constructor
-@AllArgsConstructor // Lombok: Creates a constructor with all arguments
+@NoArgsConstructor 
+@AllArgsConstructor 
 public class ResponsePayload {
     private boolean is_success;
     private String user_id;
@@ -16,7 +17,7 @@ public class ResponsePayload {
     private List<String> odd_numbers;
     private List<String> even_numbers;
     private List<String> alphabets;
-    private List<String> special_characters; // The PDF has a typo "sepcial_characters", but we'll use the correct spelling.
+    private List<String> special_characters;
     private String sum;
     private String concat_string;
 }
